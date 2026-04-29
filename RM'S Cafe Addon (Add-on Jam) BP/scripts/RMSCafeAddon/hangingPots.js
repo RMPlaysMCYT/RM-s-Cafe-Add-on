716e10e31b0e2c24f3df77152d03f8f1443fbe5c
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-04-29 11:39:34 +0800

    add

--- RM'S Cafe Addon (Add-on Jam) BP/scripts/RMSCafeAddon/hangingPots.js
+++ RM'S Cafe Addon (Add-on Jam) BP/scripts/RMSCafeAddon/hangingPots.js
@@ -1,5 +1,11 @@
 import { world, BlockPermutation, ItemStack, GameMode, system } from "@minecraft/server";
 
 world.beforeEvents.world.initialize.subscribe((event) => {
-    
+
 })
+
+class HangingPot{
+    onPlayerInteract(event){
+        
+    }
+}
